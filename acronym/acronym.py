da40d8d010d01acd4c9fc3c607678e8bba26b07b
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-29 14:42:14 -0700

    Update to the acronym.py file wth a correction and a slight bit of optimization

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -8,3 +8,8 @@ def acronym(in_string):
         if word.lower() not in forbidden:
             output += word[0].upper()
     return output
+
+
+def acronym2(in_string):
+    forbidden = ['a', 'for', 'an', 'and', 'of', 'or', 'the', 'to', 'with']
+    return ''.join(map(lambda x: x[0].upper() if x.lower() not in forbidden else '', in_string.split(' ')))
