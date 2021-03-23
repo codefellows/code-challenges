4b63d195968e0e5f5d7f455ba12a3d6c220710d5
Jacob Knaack <jacobknck@gmail.com>
2021-03-23 13:31:20 -0700

    Update node-deletion/nodeDeletion.js
    Co-authored-by: Brook Riggio           <brook@codefellows.com>

--- node-deletion/nodeDeletion.js
+++ node-deletion/nodeDeletion.js
@@ -16,7 +16,7 @@ class LinkedList {
 /**
  * Remove a node in the middle of a singly linked list given only the value of the node to remove
  *  O(1) time complexity, at worst case we need to look at every node in the list.
- *  O(1) space complexity, we create one new variable next that should be able to , 
+ *  O(1) space complexity, we create one new variable: next
  * @param {Node} node - the object to be removed from the list.
  */
 function removeNode(node) {
